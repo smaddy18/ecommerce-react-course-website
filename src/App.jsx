23a0c7f1911b5d3d8f7d91d084b403c5c3126404
Konan KOUAME <konan.kouame@imt-atlantique.net>
2026-02-12 11:42:27 +0100

    added Auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Home from "./pages/Home.jsx";
 import Auth from "./pages/Auth.jsx";
 import Checkout from "./pages/Checkout.jsx";
 import Navbar from "./components/Navbar.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
 
 function App() {
 
   return (
-      <div className="App">
-          <Navbar />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/auth" element={<Auth />} />
-              <Route path="/checkout" element={<Checkout />} />
-          </Routes>
-      </div>
+      <AuthProvider>
+          <div className="App">
+              <Navbar />
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/auth" element={<Auth />} />
+                  <Route path="/checkout" element={<Checkout />} />
+              </Routes>
+          </div>
+      </AuthProvider>
   )
 }
 
